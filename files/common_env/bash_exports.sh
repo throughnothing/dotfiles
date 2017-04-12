3512ae9eace6cc9141fe0a090215f80202423ba2
William Wolf <throughnothing@gmail.com>
2017-04-12 11:47:11 -0700

    Update Brewfile + gitconfig

--- files/common_env/bash_exports.sh
+++ files/common_env/bash_exports.sh
@@ -2,7 +2,7 @@ export EDITOR="vim"
 
 # PATHS
 export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules:/usr/local/share/npm/lib/node_modules
-export PATH="/usr/local/bin:/usr/local/sbin:$PATH:$HOME/.scripts:/var/lib/gems/1.8/bin:/usr/local/share/npm/bin"
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH:$HOME/.scripts:/var/lib/gems/1.8/bin:/usr/local/share/npm/bin:$HOME/.local/bin"
 
 # don't put duplicate lines in the history. See bash(1) for more options
 export HISTCONTROL=ignoredups:ignorespace
